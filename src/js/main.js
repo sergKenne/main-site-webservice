749e0dd9d4509615693f66327bf42e131dfeca76
Serge Simplice Kenne <kenne_serge@mail.ru>
2022-02-21 00:05:15 +0300

    завершена верстка блоги главной страницы

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,21 @@ $(document).ready(function(){
     autoplay: true,
     arrows: false
   });
+
+  $(".blog__partenaire-slider").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    arrows: true,
+  });
+
+  $(".blog__reviews-slider").slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    arrows: true,
+  });
 });
 
 
